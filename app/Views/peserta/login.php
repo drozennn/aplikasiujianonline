6cf2b987f630d379f23d089e40f9546644f11301
drozennn <rafialyahya@gmail.com>
2023-06-07 20:52:22 +0700

    Initial Commit

---
+++ app/Views/peserta/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/src/style.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <title>Sistem Lomba IMEV 12</title>
+</head>
+<body>
+    <p class="text-blue font-poppins text-blue-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto atque iure non dolorum cupiditate, quia explicabo nihil aliquid itaque quaerat. Veniam quis, quasi magnam vitae natus, unde quaerat eveniet laudantium eaque molestias, quam dolorum corrupti! Molestiae incidunt, nihil magni eum dolore inventore nesciunt ducimus libero voluptates praesentium! Maiores, consequatur autem.</p>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/Views/**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins : ['Poppins']
+      },
+    },
+  },
+  plugins: [],
+}
+
