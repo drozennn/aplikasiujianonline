0363e53fbea8aeafb440c8c53e5b86543a5d0314
drozennn <rafialyahya@gmail.com>
2023-06-07 23:23:22 +0700

    bikin template header dan sidebar

---
+++ app/Views/peserta/dashboard.php
@@ -0,0 +1,4 @@
+<?= $this->extend('templates/main'); ?>
+<?= $this->section('content'); ?>
+    <p class="font-poppins text-2xl text-blue-400"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe autem quae officiis illum nulla cupiditate necessitatibus explicabo blanditiis quam, fugit sed? Dolorem earum ullam ipsam vero, quisquam veniam accusamus, iusto possimus hic voluptatibus iure temporibus consequatur ad quidem, obcaecati voluptate. </p>
+<?= $this->endSection(); ?>
