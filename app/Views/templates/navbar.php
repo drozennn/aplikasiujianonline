7467c236416fff9a7d5612955c72afac422f39b3
drozennn <rafialyahya@gmail.com>
2023-06-08 22:26:03 +0700

    finishing template navbar dan sidebar

---
+++ app/Views/templates/navbar.php
@@ -0,0 +1,20 @@
+<!-- Navbar -->
+<nav class="bg-gray-800 py-4 w-full fixed z-50">
+    <div class="mx-auto ">
+            <div class="flex items-center justify-between px-5">
+            <div class="text-white font-bold text-4xl">IMEV 12</div>
+            <div class="text-white font-bold text-lg">Hello, Admin</div>
+        </div>
+    </div>
+</nav>
+<!-- Navbar End -->
+
+
+<!-- Hamburger Line -->
+<div id="btnNav" class="w-10 h-10 shadow-md backdrop-blur-lg bg-slate-200 opacity-50 fixed top-1/2 flex items-center justify-center">
+        <p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+</svg>
+</p>
+    </div>
+    <!-- Hamburger Line End -->
---
+++ app/Views/templates/sidebar.php
@@ -0,0 +1,7 @@
+<div class="bg-gray-800 w-64 h-screen -translate-x-full transition fixed  sm:translate-x-0" id="sidebar">
+            <nav class="mt-20">
+                <a href="#" class="block py-2 px-4 text-white hover:bg-gray-700 text-lg">Home</a>
+                <a href="#" class="block py-2 px-4 text-white hover:bg-gray-700 text-lg">Peserta Ujian</a>
+                <a href="#" class="block py-2 px-4 text-white hover:bg-gray-700 text-lg">Hasil Ujian</a>
+            </nav>
+        </div>
